bd3219d45f3fbdea0d884e41ebd75cc6ed276f3e
shogo272 <60347512+shogo272@users.noreply.github.com>
2020-01-28 15:42:38 +0900

    Add files via upload

--- toppage.php
+++ toppage.php
@@ -31,13 +31,13 @@
     <h1 style="padding-right: 90px;">IBSS</h1>
     <form id="top_page" name="top_page" action="" method="POST">
         <div>
-          <p style="padding-right: 90px;">
+          <p style="padding-right: 100px;">
         <input type="submit" class="btn1" id="btn1" formaction="order_input.php" value="注文受付">
         <input type="submit" class="btn2" id="btn2" formaction="seat_check.php" value="座席状況">
         <input type="submit" class="btn3" id="btn3" formaction="order_management.php" value="注文管理">
       </p>
         <br>
-        <p style="padding-right: 90px;">
+        <p style="padding-right: 100px;">
         <input type="submit" class="btn4" id="btn4" formaction="seat_select.php" value="会計">
         <input type="submit" class="btn5" id="btn5" formaction="login_owner.php" value="メニュー編集">
       </p>
