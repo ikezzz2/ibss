a99f8cd8e69b9586f0dfb842ebf4279596f6ff19
ikezzz2 <60342186+ikezzz2@users.noreply.github.com>
2020-01-28 13:12:58 +0900

    a

--- today_reservation_check.php
+++ today_reservation_check.php
@@ -81,9 +81,9 @@ function draw_reservation(){
     hours = new Date().getHours() ;
     var rect_hour = start_hour - hours   ;
     var rect_finish = finish_hour - start_hour;
-    context3.strokeRect(200*( rect_hour + (start_minute / 60) + 3 ),40 + (120*(<?php echo $seatlist[$j]["seatnum"] ?> - 1)),100 * (rect_finish +  (finish_minute / 60)) - 5,70);
+    context3.strokeRect(200*( 2 + rect_hour + (start_minute / 60)),40 + (120*(<?php echo $seatlist[$j]["seatnum"] ?> - 1)),200 * (rect_finish +  (finish_minute / 60)) - 5,70);
     context3.font = "30px serif";
-    context3.fillText(<?php echo "'".$reservation["name"]."'"; ?>, 200*( 2 + rect_hour + (start_minute / 60) + 1) + 10, 40 + (120*(<?php echo $seatlist[$j]["seatnum"] ?> - 1)) + 35);
+    context3.fillText(<?php echo "'".$reservation["name"]."'"; ?>, 200*( 2 + rect_hour + (start_minute / 60)) + 10, 40 + (120*(<?php echo $seatlist[$j]["seatnum"] ?> - 1)) + 35);
     <?php }
   }
 
@@ -138,7 +138,7 @@ if (canvas.getContext) {
     settime = hours +  4;
     for(var i = 30;i < height ; i += (height/number_of_seat) ){
       if(hours + (j / 200) - 3 >= 24){
-        context.strokeRect(200,i,200 * (settime - hours ) ,90);
+        context.strokeRect(200,i,200 * (settime - hours -1) ,90);
       }else{
         context.strokeRect(200,i,1000,90);
       }
